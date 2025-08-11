8f1b60e8b276137b65195b52951999bea21c991e
AntonAnisimovMIPT <anisimov-anton-main@mail.ru>
2025-08-11 22:49:37 +0300

    aos/soa first benchmark

---
+++ benches/aos_soa.rs
@@ -0,0 +1,78 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use rand::prelude::*;
+
+#[allow(dead_code)]
+struct SomeBankAccount {
+    full_name: String,
+    balance: u128,
+}
+
+impl SomeBankAccount {
+    fn new(full_name: String, balance: u128) -> Self {
+        SomeBankAccount { full_name, balance }
+    }
+}
+
+struct LotsOfBankAccounts {
+    full_names: Vec<String>,
+    balances: Vec<u128>,
+}
+
+#[allow(dead_code)]
+impl LotsOfBankAccounts {
+    fn new() -> Self {
+        Self {
+            full_names: Vec::new(),
+            balances: Vec::new(),
+        }
+    }
+
+    fn with_capacity(capacity: usize) -> Self {
+        Self {
+            full_names: Vec::with_capacity(capacity),
+            balances: Vec::with_capacity(capacity),
+        }
+    }
+
+    fn push(&mut self, full_name: String, balance: u128) {
+        self.full_names.push(full_name);
+        self.balances.push(balance);
+    }
+}
+
+fn generate_test_data_aos_soa(size: usize) -> (Vec<SomeBankAccount>, LotsOfBankAccounts) {
+    let mut aos = Vec::with_capacity(size);
+    let mut soa = LotsOfBankAccounts::with_capacity(size);
+    let mut rng = rand::rng();
+    let mut nums: Vec<u128> = (1..1000).collect();
+    nums.shuffle(&mut rng);
+
+    for _ in 0..size {
+        let name = "mock_user".to_string() + &rng.random::<char>().to_string();
+        let balance = nums.choose(&mut rng).unwrap();
+        aos.push(SomeBankAccount::new(name.clone(), *balance));
+        soa.push(name, *balance);
+    }
+
+    (aos, soa)
+}
+
+fn sum_balances_aos(accounts: &[SomeBankAccount]) -> u64 {
+    accounts.iter().map(|x| x.balance as u64).sum()
+}
+
+fn sum_balances_soa(accounts: &LotsOfBankAccounts) -> u64 {
+    accounts.balances.iter().map(|&x| x as u64).sum()
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let size = 2_000_000;
+    let (aos, soa) = generate_test_data_aos_soa(size);
+
+    c.bench_function("sum_balances_aos_2M", |b| b.iter(|| sum_balances_aos(&aos)));
+
+    c.bench_function("sum_balances_soa_2M", |b| b.iter(|| sum_balances_soa(&soa)));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Blazing Fa(Ru)st");
+}
