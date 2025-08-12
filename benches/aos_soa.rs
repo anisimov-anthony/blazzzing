406b22cb7d45561f2424e887bee6792b0a54caea
AntonAnisimovMIPT <anisimov-anton-main@mail.ru>
2025-08-12 13:01:31 +0300

    more benches for aos/soa

--- benches/aos_soa.rs
+++ benches/aos_soa.rs
@@ -65,14 +65,53 @@ fn sum_balances_soa(accounts: &LotsOfBankAccounts) -> u64 {
     accounts.balances.iter().map(|&x| x as u64).sum()
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let size = 2_000_000;
+fn criterion_benchmark_1k(c: &mut Criterion) {
+    let size = 1_000;
     let (aos, soa) = generate_test_data_aos_soa(size);
 
-    c.bench_function("sum_balances_aos_2M", |b| b.iter(|| sum_balances_aos(&aos)));
+    c.bench_function("sum_balances_aos_1k", |b| b.iter(|| sum_balances_aos(&aos)));
 
-    c.bench_function("sum_balances_soa_2M", |b| b.iter(|| sum_balances_soa(&soa)));
+    c.bench_function("sum_balances_soa_1k", |b| b.iter(|| sum_balances_soa(&soa)));
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+fn criterion_benchmark_10k(c: &mut Criterion) {
+    let size = 10_000;
+    let (aos, soa) = generate_test_data_aos_soa(size);
+
+    c.bench_function("sum_balances_aos_10k", |b| {
+        b.iter(|| sum_balances_aos(&aos))
+    });
+
+    c.bench_function("sum_balances_soa_10k", |b| {
+        b.iter(|| sum_balances_soa(&soa))
+    });
+}
+
+fn criterion_benchmark_100k(c: &mut Criterion) {
+    let size = 100_000;
+    let (aos, soa) = generate_test_data_aos_soa(size);
+
+    c.bench_function("sum_balances_aos_100k", |b| {
+        b.iter(|| sum_balances_aos(&aos))
+    });
+
+    c.bench_function("sum_balances_soa_100k", |b| {
+        b.iter(|| sum_balances_soa(&soa))
+    });
+}
+
+fn criterion_benchmark_1m(c: &mut Criterion) {
+    let size = 1_000_000;
+    let (aos, soa) = generate_test_data_aos_soa(size);
+
+    c.bench_function("sum_balances_aos_1M", |b| b.iter(|| sum_balances_aos(&aos)));
+
+    c.bench_function("sum_balances_soa_1M", |b| b.iter(|| sum_balances_soa(&soa)));
+}
+
+criterion_group!(benches_1k, criterion_benchmark_1k);
+criterion_group!(benches_10k, criterion_benchmark_10k);
+criterion_group!(benches_100k, criterion_benchmark_100k);
+criterion_group!(benches_1m, criterion_benchmark_1m);
+
+criterion_main!(benches_1k, benches_10k, benches_100k, benches_1m);
